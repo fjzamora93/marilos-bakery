e72d6b5bedac7821d0be394736735d34f5febce9
fjzamora93 <fjzamora93@gmail.com>
2025-07-22 12:51:17 +0200

    modified style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,37 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{html,ts,js,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': {
+          DEFAULT: '#22C55E', // green-500
+          hover: '#16A34A',  // green-600
+          darker: '#15803D', // green-700
+          lighter: '#abe0beff', // green-700
+
+        },
+        'secondary-color': {
+          DEFAULT: '#2522c5ff', 
+          hover: '#4b5d9bff',  
+          darker: '#0d172bff', 
+          lighter: '#8db1e0ff', 
+
+        },
+        'accent-red': {
+          DEFAULT: '#EF4444', // red-500
+          hover: '#DC2626',  // red-600
+          darker: '#B91C1C', // red-700
+          lighter: '#eb9595ff', 
+
+        },
+        // Define tus otros colores de gradiente aquí
+      },
+      // Puedes extender otros aspectos del tema como spacing, fontSize, etc.
+    },
   },
   plugins: [],
 }
-
