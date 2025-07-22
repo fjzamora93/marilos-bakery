d0c72076172a073fb5469301aa86eaf705734164
fjzamora93 <fjzamora93@gmail.com>
2025-07-22 19:31:06 +0200

    update style

--- src/app/shared/components/hero-section/hero-section.component.ts
+++ src/app/shared/components/hero-section/hero-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { FloatingButtonComponent } from "../floating-button/floating-button.component";
 
 @Component({
   standalone: true,
   selector: 'app-hero-section',
-  imports: [TranslateModule],
+  imports: [TranslateModule, FloatingButtonComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss'
 })
--- src/app/shared/pages/home/home.component.ts
+++ src/app/shared/pages/home/home.component.ts
@@ -4,12 +4,13 @@ import { APP_NAME } from '@app/shared/constants/app.constants';
 import { HeroSectionComponent } from "@app/shared/components/hero-section/hero-section.component";
 import { FeaturedProductsComponent } from "@app/shared/components/featured-products/featured-products.component";
 import { AboutSectionComponent } from "@app/shared/components/about-section/about-section.component";
+import { FloatingButtonComponent } from "@app/shared/components/floating-button/floating-button.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TranslateModule, HeroSectionComponent, FeaturedProductsComponent, AboutSectionComponent],
+  imports: [TranslateModule, HeroSectionComponent, FeaturedProductsComponent, AboutSectionComponent, FloatingButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
